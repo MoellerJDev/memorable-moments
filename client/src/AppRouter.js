bb668178c7024fd28bd94c01ec23386e986662d7
Joshua <josh.t.m@icloud.com>
2023-05-23 17:48:58 -0500

    Update READMEs, implement components

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
-// Import your components here
 import Home from './components/Home';
-import UserProfile from './components/UserProfile';
 import PhotoUpload from './components/PhotoUpload';
+import UserProfile from './components/UserProfile';
 
 const AppRouter = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/profile" component={UserProfile} />
         <Route exact path="/upload" component={PhotoUpload} />
+        <Route exact path="/profile" component={UserProfile} />
       </Switch>
     </Router>
   );
