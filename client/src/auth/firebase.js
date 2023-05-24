b26f33e82fa5425180829159702fa1b70e22a941
MoellerJDev <josh.t.m@outlook.com>
2023-05-24 14:44:16 -0500

    Factor out info for privacy

--- client/src/auth/firebase.js
+++ client/src/auth/firebase.js
@@ -1,16 +1,7 @@
 import { initializeApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCvAEosw5tCUKcLYWyHhavTlqrMdiUIjZI",
-  authDomain: "memorable-moments-9f679.firebaseapp.com",
-  projectId: "memorable-moments-9f679",
-  storageBucket: "memorable-moments-9f679.appspot.com",
-  messagingSenderId: "438400251278",
-  appId: "1:438400251278:web:d29597e8944811eb04e0df",
-  measurementId: "G-1LK5ZVJD9S"
-};
+import { firebaseConfig } from './FirebaseConfig';
 
 let app;
 
