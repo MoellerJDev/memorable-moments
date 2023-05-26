5929102736ac801b3cd79e7454ba191b39563770
MoellerJDev <josh.t.m@outlook.com>
2023-05-26 14:32:30 -0500

    Fix issue, sign in/sign out modals working

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,5 +1,5 @@
 import React, { useCallback, useContext, useState } from 'react';
-import { useHistory, Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom'; // Removed history from here
 import { auth } from '../auth/firebase';
 import { AuthContext } from '../auth/AuthContext';
 import { signInWithEmailAndPassword } from 'firebase/auth';
@@ -7,8 +7,7 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 
-const SignIn = () => {
-  const history = useHistory();
+const SignIn = ({ history, handleClose}) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
@@ -18,17 +17,19 @@ const SignIn = () => {
       event.preventDefault();
       try {
         await signInWithEmailAndPassword(auth, email, password);
+        handleClose();
         history.push('/');
       } catch (error) {
         setErrorMessage(error.message);
       }
     },
-    [history, email, password]
+    [history, handleClose, email, password]
   );
 
   const { currentUser } = useContext(AuthContext);
 
   if (currentUser) {
+    handleClose();
     return <Redirect to="/" />;
   }
 
